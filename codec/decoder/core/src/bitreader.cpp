09a0343865663383f93a8f1142ecf3bd83843997
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2015-08-12 01:37:02 -0700

    make it work on OS X and with clang

--- codec/decoder/core/src/bitreader.cpp
+++ codec/decoder/core/src/bitreader.cpp
@@ -12,8 +12,25 @@
 //#include "./vpx_config.h"
 
 #include "bitreader.h"
+#ifdef __APPLE__
+#include <libkern/OSByteOrder.h>
+# define htobe64 OSSwapHostToBigInt64
+# define be64toh OSSwapBigToHostInt64
+# define htobe32 OSSwapHostToBigInt32
+# define be32toh OSSwapBigToHostInt32
+# define htobe16 OSSwapHostToBigInt16
+# define be16toh OSSwapBigToHostInt16
+
+# define htole64 OSSwapHostToLittleInt64
+# define le64toh OSSwapLittleToHostInt64
+# define htole32 OSSwapHostToLittleInt32
+# define le32toh OSSwapLittleToHostInt32
+# define htole16 OSSwapHostToLittleInt16
+# define le16toh OSSwapLittleToHostInt16
+#else
 #define _BSD_SOURCE       /* See feature_test_macros(7) */
 #include <endian.h>
+#endif
 //#include "vpx_dsp/prob.h"
 //#include "vpx_ports/mem.h"
 //#include "vpx_mem/vpx_mem.h"
