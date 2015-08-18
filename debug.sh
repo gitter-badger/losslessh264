9aceed75db8388ad892614e1f7d02a4af18ed38d
Ken Elkabany <ken@elkabany.com>
2015-08-18 01:21:57 -0700

    WIP

--- debug.sh
+++ debug.sh
@@ -4,4 +4,4 @@ echo "File" ${f}
 grep ^R "${f}" >> "${f}_R"
 grep ^W "${f}" >> "${f}_W"
 cat "${f}_R" | tr R W > "${f}_RW"
-echo "Run 'diff ${f}_W ${f}_R'"
+echo "Run 'diff ${f}_W ${f}_RW'"
--- roundtriptest/run.sh
+++ roundtriptest/run.sh
@@ -9,8 +9,7 @@ pushd "$BASEDIR/.."
 ./piedpiper_make
 popd
 
-#FILES="$BASEDIR/tibby.264 $BASEDIR/black.264 $BASEDIR/../res/BAMQ2_JVC_C.264 $BASEDIR/../res/BA1_FT_C.264"
-FILES="$BASEDIR/tibby.264"
+FILES="$BASEDIR/tibby.264 $BASEDIR/black.264 $BASEDIR/../res/BAMQ2_JVC_C.264 $BASEDIR/../res/BA1_FT_C.264"
 
 for f in "$@" $FILES; do
     rm -f /tmp/a.pip* /tmp/a.264
