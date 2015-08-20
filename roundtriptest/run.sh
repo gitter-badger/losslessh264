8a7b16b1f42d4c84cc9f6745a8e92e81c767c85d
Daniel Sommermann <dcsommer@dropbox.com>
2015-08-20 16:31:45 -0700

    Allow specification of additional files to roundtrip

--- roundtriptest/run.sh
+++ roundtriptest/run.sh
@@ -11,6 +11,16 @@ popd
 
 FILES="$BASEDIR/tibby.264 $BASEDIR/black.264 $BASEDIR/../res/BAMQ2_JVC_C.264 $BASEDIR/../res/BA1_FT_C.264"
 
+if [ "$#" -eq 1 ]
+then
+    SAVEIFS=$IFS
+    IFS=$(echo -en "\n\b")
+    for x in $(ls $1); do
+        FILES+=$x
+    done
+    IFS=$SAVEIFS
+fi
+
 for f in "$@" $FILES; do
     rm -f /tmp/a.pip* /tmp/a.264
     echo "============== $f ================"
