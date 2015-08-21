64071e89447286e7db79755eb8f1228a7a7b8a55
Daniel Sommermann <dcsommer@dropbox.com>
2015-08-21 10:00:17 -0700

    Fix run.sh for directories

--- roundtriptest/run.sh
+++ roundtriptest/run.sh
@@ -9,19 +9,20 @@ pushd "$BASEDIR/.."
 ./piedpiper_make
 popd
 
-FILES="$BASEDIR/tibby.264 $BASEDIR/black.264 $BASEDIR/../res/BAMQ2_JVC_C.264 $BASEDIR/../res/BA1_FT_C.264"
+FILES=($BASEDIR/tibby.264 $BASEDIR/black.264 $BASEDIR/../res/BAMQ2_JVC_C.264 $BASEDIR/../res/BA1_FT_C.264)
 
 if [ "$#" -eq 1 ]
 then
     SAVEIFS=$IFS
     IFS=$(echo -en "\n\b")
-    for x in $(ls $1); do
-        FILES+=$x
+    for f in $(ls $1); do
+        FILES+=("$1$f")
     done
     IFS=$SAVEIFS
 fi
 
-for f in "$@" $FILES; do
+IFS=""
+for f in ${FILES[@]}; do
     rm -f /tmp/a.pip* /tmp/a.264
     echo "============== $f ================"
     echo "    ./h264dec $f /tmp/a.pip"
